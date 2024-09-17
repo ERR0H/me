5540befef66f39c298d6009281592a1604181af4
@404. <87565477+ERR0H@users.noreply.github.com>
2024-09-17 18:17:31 +0000

    update

--- data/fetch.js
+++ data/fetch.js
@@ -3,3 +3,17 @@ const div = document.querySelector("#user-pf");
 
 fetch(file)
     .then(Response => Response.json())
+    .then(data => {
+        data.forEach(user => {
+            const u = document.createElement("div");
+            u.classList.add("user-pf");
+            u.innerHTML = `
+            <img class="banner" src="${user.banner}" alt="">
+            <div class="av-na">
+            <img class="avatar" src="${user.avatar}" alt="">
+            <h2>${user.name}</h2>
+            </div>
+            `;
+            div.appendChild(u);
+        });
+    })
